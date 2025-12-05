1d0c3a1a8349021b68422935424b646af82a5a1e
iCoderPotato <cesarjosuealvizar@hotmail.com>
2025-12-04 19:37:26 -0600

    Rename UnitTestApplication to IntegrationTestApplication

--- test/Application.UnitTest/UnitTestApplication.cs
+++ test/Application.UnitTest/UnitTestApplication.cs
@@ -1,10 +1,19 @@
-﻿namespace Application.UnitTest;
+namespace Application.IntegrationTest;
 
-public class UnitTest1
+public class IntegrationTestApplication
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(4, 4, 4, 50)]
+    [InlineData(5, 5, 5, 100)]
+    [InlineData(6, 6, 6, 150)]
+    public void TestVolumen(double BaseMenor, double BaseMayor, double altura, double Volumen)
     {
+        // Arrange - Set up
+        
+        // Act - Perform
+        double resultado = Application.Figura261E05.Volumen(area, altura);
+        // Assert - Verify
+        Assert.Equal(resultado, Volumen);
 
     }
 }
